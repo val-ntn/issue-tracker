c3b574219718d7353f8aa38354c5f97fecacfc5b
val-ntn <valw108@gmail.com>
2025-04-13 15:10:11 +0200

    End Chapter 2

---
+++ public/App.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// App.jsx
+var element = /*#__PURE__*/React.createElement("div", {
+  title: "Outer div"
+}, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
+ReactDOM.render(element, document.getElementById('contents'));
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// App.jsx
+const element = (
+    <div title="Outer div">
+      <h1>Hello World!</h1>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('contents'));
+  
